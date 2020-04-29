13bb598bca89c4fa5f463502baf0dbc6240a4de4
Mithun Jothiravi [I] <mjothiravi@tesla.com>
2020-04-29 15:18:26 -0400

    Assignment submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming -> Week 3 -> Programming Assignment 2: Lexical
 
-## Write a short comment describing this function
+###########################################
+## Input: Valid square invertible matrix
 
+## Functonality:
+## - setReg(): Set non-inverted matrix data.
+## - getReg(): Get non-inverted matrix data.
+## - setInv(): Set inverted matrix data.
+## - getInv(): Get inverted matrix data.
+
+## Return: Special matrix object that can cache it's inverse.
+###########################################
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # Declare inverse matrix with NULL initial value
+  invMatrix <- NULL
+  
+  # Set non-inverted matrix data
+  setReg <- function(inputMatrix) {
+    x <<- inputMatrix
+    invMatrix <<- NULL
+  }
+  
+  # Get non-inverted matrix data
+  getReg <- function() {
+    return(x)
+  }
+  
+  # Set inverted matrix data
+  setInv <- function(inputMatrix) {
+    invMatrix <<- inputMatrix
+  }
+  
+  # Get inverted matrix data
+  getInv <- function() {
+    return(invMatrix)
+  }
+  
+  # Return the methods associated with the makeCacheMatrix object.
+  return(list(setReg = setReg, getReg = getReg, setInv = setInv, getInv = getInv))
 
 }
 
 
-## Write a short comment describing this function
+###########################################
+## Input: Cache matrix object.
 
+## Functonality:
+## - If inversae has been calculated, and the matrix has not
+## changed, then return the cached inverse matrix.
+## - Otherwise, calculate the inverse matrix, store the data
+## and return the inverted matrix result.
+
+## Return: Inverted matrix.
+##########################################
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  invMatrix <- x$getInv()
+  
+  if (!is.null(invMatrix)) {
+    print("Identified valid inverse matrix...")
+    return(invMatrix)
+  }
+  
+  print("Inverse matrix resulted in NULL.")
+  regMatrix <- x$getReg()
+  invMatrix <- solve(regMatrix)
+  
+  x$setInv(invMatrix)
+  
+  return(invMatrix)
+    
 }
